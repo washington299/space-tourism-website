dc55aacb243c358dce9ed020293273f3a35b5536
Washington Campos <washingtoncampos9@gmail.com>
2022-01-08 18:09:25 -0300

    test: ensure header element exists and menu mobile toggle is working

---
+++ src/components/Header/test.tsx
@@ -0,0 +1,29 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+
+import { Header } from ".";
+
+describe("<Header />", () => {
+	it("Should render header element and logo", () => {
+		render(<Header />);
+
+		expect(screen.getByRole("banner")).toBeInTheDocument();
+		expect(screen.getByLabelText(/Logo/i)).toBeInTheDocument();
+	});
+
+	it("Should toggle menu navigation", () => {
+		render(<Header />);
+
+		expect(screen.getByLabelText(/Menu hamburger/i)).toBeInTheDocument();
+		expect(screen.queryByTestId(/menu mobile/i)).not.toBeInTheDocument();
+
+		fireEvent.click(screen.getByLabelText(/Menu hamburger/i));
+
+		expect(screen.getByTestId(/menu mobile/i)).toBeInTheDocument();
+		expect(screen.queryByLabelText(/Menu hamburger/i)).not.toBeInTheDocument();
+
+		fireEvent.click(screen.getByLabelText(/Close icon/i));
+
+		expect(screen.getByLabelText(/Menu hamburger/i)).toBeInTheDocument();
+		expect(screen.queryByTestId(/menu mobile/i)).not.toBeInTheDocument();
+	});
+});

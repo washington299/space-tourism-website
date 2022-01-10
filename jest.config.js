4b1f71f378368cf9572705440a538f7ff354c96b
Washington Campos <washingtoncampos9@gmail.com>
2022-01-10 13:34:54 -0300

    chore: remove collect coverage

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
 module.exports = {
 	testEnvironment: "jsdom",
 	testPathIgnorePatterns: ["/node_modules/", "/.next/"],
-	collectCoverage: true,
-	collectCoverageFrom: ["src/**/*.ts(x)", "!src/pages/**"],
 	setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"],
 	modulePaths: ["<rootDir>/src"],
 };

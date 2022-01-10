d2b7f36fa34a39de5c3a1293046136c58201c308
Washington Campos <washingtoncampos9@gmail.com>
2022-01-10 18:09:19 -0300

    chore: add templates to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-	content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx}",
+		"./src/components/**/*.{js,ts,jsx,tsx}",
+		"./src/templates/**/*.{js,ts,jsx,tsx}",
+	],
 	theme: {
 		colors: {
 			dark: "#0B0D17",

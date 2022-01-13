97ba6e3a433f0147ce85c36aafb4ee68c7c0b197
Washington Campos <washingtoncampos9@gmail.com>
2022-01-13 18:14:21 -0300

    feat: style destination page elements on large devices

--- src/templates/Destination/index.tsx
+++ src/templates/Destination/index.tsx
@@ -26,55 +26,61 @@ export const DestinationTemplate = ({ destinations }: DestinationsTypes) => {
 
 	return (
 		<Layout backgroundImage={destinationBackgroundImage}>
-			<main className="px-6">
+			<main className="px-6 md:my-10 md:mx-auto md:max-w-[600px] lg:max-w-[80%]">
 				<section>
-					<h1 className="text-white font-barlow-condensed tracking-wider uppercase text-center">
+					<h1 className="text-white font-barlow-condensed tracking-wider uppercase text-center md:text-[20px] lg:text-md">
 						{" "}
 						<span className="text-gray font-bold inline-block mr-2">01</span> Pick your destination
 					</h1>
 
-					<div className="flex flex-col items-center">
-						<div className="relative w-[170px] h-[170px] mt-8">
+					<div className="flex flex-col items-center lg:flex-row lg:items-start lg:space-x-24 lg:mt-10 xl:justify-evenly">
+						<div className="relative w-[170px] h-[170px] mt-8 md:w-[300px] md:h-[300px] xl:w-[445px] xl:h-[445px]">
 							<Image src={destination?.images.png || ""} alt={destination?.name} layout="fill" />
 						</div>
 
-						<div className="flex space-x-4 mt-4">
-							{destinations?.map(({ name }) => (
-								<button
-									key={name}
-									className={`font-barlow-condensed tracking-wide uppercase p-1 ${
-										currentDestination === name ? activeButtonStyle : disableButtonStyle
-									}`}
-									onClick={() => setCurrentDestination(name)}
-								>
-									{name}
-								</button>
-							))}
-						</div>
+						<div className="flex flex-col items-center lg:flex-1 lg:items-start xl:max-w-[600px]">
+							<div className="flex space-x-4 mt-4">
+								{destinations?.map(({ name }) => (
+									<button
+										key={name}
+										className={`font-barlow-condensed tracking-wide uppercase p-1 ${
+											currentDestination === name ? activeButtonStyle : disableButtonStyle
+										}`}
+										onClick={() => setCurrentDestination(name)}
+									>
+										{name}
+									</button>
+								))}
+							</div>
 
-						<h2 className="font-bellefair text-white text-xl uppercase mt-4">
-							{destination?.name}
-						</h2>
-						<p className="text-center text-light">{destination?.description}</p>
+							<h2 className="font-bellefair text-white text-xl uppercase mt-4 md:text-xl xl:text-2xl">
+								{destination?.name}
+							</h2>
+							<p className="text-center text-light lg:text-left xl:text-[20px]">
+								{destination?.description}
+							</p>
 
-						<div className="h-[1px] w-full bg-gray my-6" />
+							<div className="h-[1px] w-full bg-gray my-6" />
 
-						<div className="flex flex-col uppercase">
-							<span className="text-light text-center font-barlow-condensed tracking-wide">
-								Avg. distance
-							</span>
-							<span className="text-md text-white text-center font-bellefair">
-								{destination?.distance}
-							</span>
-						</div>
+							<div className="w-full md:flex md:items-center md:justify-evenly">
+								<div className="flex flex-col uppercase">
+									<span className="text-light text-center font-barlow-condensed tracking-wide">
+										Avg. distance
+									</span>
+									<span className="text-md text-white text-center font-bellefair">
+										{destination?.distance}
+									</span>
+								</div>
 
-						<div className="flex flex-col my-6 uppercase">
-							<span className="text-light text-center font-barlow-condensed tracking-wide">
-								Est. travel time
-							</span>
-							<span className="text-md text-white text-center font-bellefair">
-								{destination?.travel}
-							</span>
+								<div className="flex flex-col my-6 uppercase">
+									<span className="text-light text-center font-barlow-condensed tracking-wide">
+										Est. travel time
+									</span>
+									<span className="text-md text-white text-center font-bellefair">
+										{destination?.travel}
+									</span>
+								</div>
+							</div>
 						</div>
 					</div>
 				</section>

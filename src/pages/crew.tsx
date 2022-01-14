27895cee68bddbbd0293b1b7421892815350f071
Washington Campos <washingtoncampos9@gmail.com>
2022-01-14 17:30:14 -0300

    feat: create crew template elements

--- src/pages/crew.tsx
+++ src/pages/crew.tsx
@@ -1,7 +1,23 @@
+import { GetStaticProps } from "next";
+
+import { QUERY } from "services/queries";
+
 import { CrewTemplate } from "templates/Crew";
 
-const Crew = () => {
-	return <CrewTemplate />;
+import { CrewTypes } from "types/crew";
+
+const Crew = ({ crew }: CrewTypes) => {
+	return <CrewTemplate crew={crew} />;
 };
 
 export default Crew;
+
+export const getStaticProps: GetStaticProps = async () => {
+	const { data } = await QUERY.getCrews();
+
+	return {
+		props: {
+			crew: data,
+		},
+	};
+};
--- src/templates/Crew/index.tsx
+++ src/templates/Crew/index.tsx
@@ -1,8 +1,12 @@
+import Image from "next/image";
+
 import { Layout } from "components/Layout";
 
 import { useWindowSize } from "hooks/useWindowSize";
 
-export const CrewTemplate = () => {
+import { CrewTypes } from "types/crew";
+
+export const CrewTemplate = ({ crew }: CrewTypes) => {
 	const { width } = useWindowSize();
 
 	const crewBackgroundImage =
@@ -14,11 +18,32 @@ export const CrewTemplate = () => {
 
 	return (
 		<Layout backgroundImage={crewBackgroundImage}>
-			<main className="px-6">
+			<main className="px-6 pb-6">
 				<h1 className="text-white font-barlow-condensed tracking-wider uppercase text-center md:text-[20px] lg:text-md">
 					{" "}
 					<span className="text-gray font-bold inline-block mr-2">02</span> Meet your crew
 				</h1>
+
+				<div className="flex flex-col items-center">
+					<div className="relative w-[150px] h-[220px] mt-8">
+						<Image src={crew[0].images.png} alt={crew[0].name} layout="fill" />
+					</div>
+
+					<div className="w-full h-[1px] border-b-[1px] border-gray" />
+
+					<div className="flex space-x-4 mt-8">
+						<button className="h-[10px] w-[10px] bg-gray rounded-full" />
+						<button className="h-[10px] w-[10px] bg-gray rounded-full" />
+						<button className="h-[10px] w-[10px] bg-gray rounded-full" />
+						<button className="h-[10px] w-[10px] bg-gray rounded-full" />
+					</div>
+
+					<section className="flex flex-col items-center mt-8">
+						<div className="text-gray uppercase font-bellefair">{crew[0].role}</div>
+						<h2 className="text-white text-[24px] uppercase font-bellefair">{crew[0].name}</h2>
+						<p className="text-light text-xs text-center font mt-4">{crew[0].bio}</p>
+					</section>
+				</div>
 			</main>
 		</Layout>
 	);

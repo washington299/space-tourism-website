cdb568b7e21bfc504b9209499675668d802e6822
Washington Campos <washingtoncampos9@gmail.com>
2022-01-14 15:59:18 -0300

    feat: create crew api endpoint

---
+++ src/pages/api/crews.ts
@@ -0,0 +1,7 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import data from "../../../data.json";
+
+export default function handler(_req: NextApiRequest, res: NextApiResponse) {
+	res.status(200).json(data.crew);
+}

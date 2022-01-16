3bc011c358b27d1c3b0dd26af3c21c671dcf49f9
Washington Campos <washingtoncampos9@gmail.com>
2022-01-16 18:03:37 -0300

    feat: get technology data and create elements

--- src/pages/technology.tsx
+++ src/pages/technology.tsx
@@ -1,7 +1,23 @@
+import { GetStaticProps } from "next";
+
+import { QUERY } from "services/queries";
+
 import { TechnologyTemplate } from "templates/Technology";
 
-const Technology = () => {
-	return <TechnologyTemplate />;
+import { TechnologyTypes } from "types/technology";
+
+const Technology = ({ technology }: TechnologyTypes) => {
+	return <TechnologyTemplate technology={technology} />;
 };
 
 export default Technology;
+
+export const getStaticProps: GetStaticProps = async () => {
+	const { data } = await QUERY.getTechnology();
+
+	return {
+		props: {
+			technology: data,
+		},
+	};
+};
--- src/templates/Technology/index.tsx
+++ src/templates/Technology/index.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
+import Image from "next/image";
+
 import { Layout } from "components/Layout";
 
 import { useWindowSize } from "hooks/useWindowSize";
 
-export const TechnologyTemplate = () => {
+import { TechnologyTypes } from "types/technology";
+
+export const TechnologyTemplate = ({ technology }: TechnologyTypes) => {
+	const [currentTechnology] = useState("Launch vehicle");
+
 	const { width } = useWindowSize();
+
+	const filteredTechnology = technology.find(item => item.name === currentTechnology);
 
 	const technologyBackgroundImage =
 		width < 768
@@ -14,7 +23,52 @@ export const TechnologyTemplate = () => {
 
 	return (
 		<Layout backgroundImage={technologyBackgroundImage}>
-			<>Technology template</>
+			<main className="mb-6">
+				<section>
+					<h1 className="text-white font-barlow-condensed tracking-wider uppercase text-center md:text-[20px] lg:text-md">
+						{" "}
+						<span className="text-gray font-bold inline-block mr-2">03</span> Space launch 101
+					</h1>
+
+					<div className="mt-8">
+						<div className="relative h-[170px]">
+							<Image
+								src={
+									width < 768
+										? filteredTechnology?.images.landscape || ""
+										: filteredTechnology?.images.portrait || ""
+								}
+								alt={filteredTechnology?.name}
+								layout="fill"
+							/>
+						</div>
+
+						<div className="px-6">
+							<div className="flex justify-center space-x-4 mt-8">
+								<button className="w-[40px] h-[40px] text-white border border-gray rounded-full">
+									1
+								</button>
+								<button className="w-[40px] h-[40px] text-white border border-gray rounded-full">
+									2
+								</button>
+								<button className="w-[40px] h-[40px] text-white border border-gray rounded-full">
+									3
+								</button>
+							</div>
+
+							<section className="flex flex-col items-center mt-8">
+								<div className="text-light text-xs font-barlow-condensed tracking-wide uppercase">
+									The terminology...
+								</div>
+								<h2 className="text-white text-[24px] font-bellefair uppercase mt-2">
+									{filteredTechnology?.name}
+								</h2>
+								<p className="text-center text-light mt-4">{filteredTechnology?.description}</p>
+							</section>
+						</div>
+					</div>
+				</section>
+			</main>
 		</Layout>
 	);
 };

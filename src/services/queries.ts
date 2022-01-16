1fdf36758814190f0d10aac302d03c27daa9d8eb
Washington Campos <washingtoncampos9@gmail.com>
2022-01-16 17:34:39 -0300

    feat: create getTechnology query

--- src/services/queries.ts
+++ src/services/queries.ts
@@ -3,4 +3,5 @@ import { api } from "services/config";
 export const QUERY = {
 	getDestinations: () => api.get("/api/destinations"),
 	getCrews: () => api.get("/api/crews"),
+	getTechnology: () => api.get("/api/technology"),
 };
